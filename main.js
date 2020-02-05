9865296c3c9f3c9e61e7352e326472c87eeb42ca
Piotr <piotr.romatowski@gmail.com>
2020-02-05 17:27:04 +0100

    js-sprawdzenie-liczby

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let n=125   
+let n=125 
 
-if (n>100,n<=200) {
+if (n>100 && n<=200) {
     console.log('Liczba znajduje się w przedziale');
 }
     else {console.log('Liczba nie znajduje się w przedziale');
